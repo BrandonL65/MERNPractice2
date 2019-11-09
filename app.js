4e53df8440c89e27827ea47a49e353ca9dd34230
Brandon <brandon.lau86@gmail.com>
2019-11-09 16:18:58 -0500

    Connect to DB done

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 let express = require("express");
 let app = express();
+let mongoose = require("mongoose");
 let PORT = 5000;
 
 
-
 //Connect to DB 
-
-
+let options = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+}
+mongoose.connect("mongodb+srv://brandon123:123@devconnection-dazbv.mongodb.net/test?retryWrites=true&w=majority", options)
+.then(() => {
+  console.log("Connected");
+}).catch(err => {
+  console.log(err);
+})
 //Controllers 
 
 
@@ -14,3 +22,5 @@ let PORT = 5000;
 
 
 //Listen
+
+app.listen(5000);
