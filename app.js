b8a6cb41889ff986dc653de77f6bd6d6e3112acc
Brandon <brandon.lau86@gmail.com>
2019-11-10 13:37:07 -0500

    Add cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let express = require("express");
 let app = express();
 let mongoose = require("mongoose");
 let Postroutes = require("./routes/PostRoutes.js");
+let cors = require("cors");
 require("dotenv").config();
 let PORT = process.env.port || 5000;
 
@@ -19,7 +20,9 @@ mongoose.connect(process.env.DB_URL, options)
 })
 //Middleware 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+
 //Controllers 
 
 
